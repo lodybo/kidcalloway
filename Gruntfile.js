c3571cf7dc209caa430c0f625020a8b2c0c1d2dd
Lody Borgers <hi@lodybo.nl>
2015-04-26 02:02:41 +0200

    Added Gruntfile to the project

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jshint']);
+
+};
