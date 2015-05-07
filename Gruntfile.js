96b6d335ed9ecdcbf1f1d5c9f2d95f5df31295d3
Lody Borgers <hi@lodybo.nl>
2015-05-08 00:23:17 +0200

    Added tasks and completed Gruntfile.js. For now.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,108 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    // HTML
+    codekit: {
+	    dist: {
+		    src: ["src/kit/**/*.kit"],
+		    dest: ["build/"]
+	    }
+    },
     
+    // Sass
+    sass: {
+	    options: {
+		    sourceMap: true,
+		    includePaths: require("node-neat").includePaths
+	    },
+	    dist: {
+		    src: ["src/scss/kid.scss"],
+		    dest: ["build/css/styles.css"]
+	    }
+    },
+    autoprefixer: {
+	    options: {
+		    browsers: ['last 3 versions', 'ie9', 'ie10'],
+		    map: true
+	    },
+	    dist: {
+		    src: ["<%= sass.dist.src %>"],
+		    dest: ["<%= sass.dist.src %>"]
+	    }
+    },
+    sassdoc: {
+	    options: {
+		    dest: ["docs/sassdoc/"]
+	    },
+	    src: ["src/scss/**/*.scss"]
+    },
+    
+    // JS
+    
+    // GENERIC
+    clean: {
+	    build: ["build"],
+	    sassdoc: ["docs/sassdoc"],
+	    jsdoc: ["docs/jsdoc"],
+	    test: ["test"]
+    },
+    connect: {
+	    build: {
+		    options: {
+			    livereload: true,
+			    base: "build/",
+			    port: 3412
+		    }
+	    },
+	    docs: {
+		    options: {
+			    base: "docs/",
+			    port: 3142
+		    }
+	    }
+    },
     watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
+      kit: {
+	      files: ["src/kit/**/*.kit"],
+	      tasks: ["kit"]
+      },
+      scss: {
+	      files: ["src/scss/**/*.kit"],
+	      tasks: ["scss", "docs:sass"],
+      },
+      js: {}
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  require('load-grunt-tasks')(grunt);
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask("kit", ["codekit"]);
+  
+  grunt.registerTask("scss", ["sass", "autoprefixer"]);
+  
+  grunt.registerTask("js", []);
+  
+  grunt.registerTask("docs", function (mode) {
+	  var taskList = ["sassdoc", "jsdoc"];
+	  var cleanList = ["clean:sassdoc", "clean:jsdoc"];
+	  
+	  if (mode && mode === "sass")
+	  {
+		  taskList.pop();
+		  cleanList.pop();
+	  }
+	  else if (mode && mode === "js")
+	  {
+		  taskList.shift();
+		  cleanList.shift();
+	  }
+	  
+	  grunt.tasks.run(cleanList);
+	  grunt.tasks.run(taskList);
+  });
+  
+  grunt.registerTask("serve", ["connect:build", "watch"]);
+  
+  grunt.registerTask("default", ["serve"]);
 
-};
+}; 
