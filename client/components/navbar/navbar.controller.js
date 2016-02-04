2da73f1a8fbf337c740c64cf18ffa6a788cdba96
Lody Borgers <lody.borgers@capgemini.com>
2016-02-04 08:28:40 +0100

    Updated menu styles and made them properly mobile-first!

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('kidCallowayApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth) {
+  .controller('NavbarCtrl', function ($scope, $location, Auth, $window) {
     $scope.menu = [{
       'title': 'Live',
       'link': '#live',
@@ -33,6 +33,25 @@ angular.module('kidCallowayApp')
     $scope.isAdmin = Auth.isAdmin;
     $scope.getCurrentUser = Auth.getCurrentUser;
     $scope.checkRestriction = Auth.checkRestriction;
+
+    // Check the window (inner)width and see if we're over our CSS border of 550px
+    $scope.overBorder = false;
+    $(window).on("resize.doResize", function (){
+      $scope.$apply(function(){
+        if ($window.innerWidth > 549) {
+          $scope.overBorder = true;
+        } else {
+          $scope.overBorder = false;
+        }
+
+        console.log($scope.overBorder);
+      });
+    });
+
+    // When we destroy our scope, we need to remove this listener otherwise we'll get a lot of issues.
+    $scope.$on("$destroy",function (){
+      $(window).off("resize.doResize"); //remove the handler added earlier
+    });
 
     $scope.logout = function() {
       Auth.logout();
