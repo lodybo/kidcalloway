ad7ef9cf58e23b8796ed1c8872a6cb23fd08d367
Lody Borgers <lody.borgers@capgemini.com>
2016-04-04 17:57:56 +0200

    Created service for agenda and created the get all functionality.

--- client/app/_services/AgendaService/AgendaService.service.js
+++ client/app/_services/AgendaService/AgendaService.service.js
@@ -1,6 +1,21 @@
 'use strict';
 
 angular.module('kidCallowayApp')
-  .service('AgendaService', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+  .service('AgendaService', function ($resource) {
+    var endpoint = $resource("/api/agenda");
+    
+    // *** Private methods
+    
+    // *** Public methods
+    var _getAll = function() {
+        // Get all records and return them
+        return endpoint.get({}).then(function (response) {
+            return response.data;
+        });
+    };
+    
+    // Return public functions
+    return {
+        get: getAll
+    };
   });
