163d20567d4e6c0bac262120539e65600655537c
Lody Borgers <lody.borgers@capgemini.com>
2016-04-12 08:18:24 +0200

    Improved link structure for navigation menu

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -4,22 +4,22 @@ angular.module('kidCallowayApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth, $window, $timeout) {
     $scope.menu = [{
       'title': 'Live',
-      'link': '#live',
+      'link': '!#live',
       'restriction': 'all',
       'icon': 'music',
     }, {
       'title': 'Birth',
-      'link': '#birth',
+      'link': '!#birth',
       'restriction': 'all',
       'icon': 'mars',
     }, {
       'title': 'Experience',
-      'link': '#experience',
+      'link': '!#experience',
       'restriction': 'all',
       'icon': 'play',
     }, {
       'title': 'Contact',
-      'link': '#contact',
+      'link': '!#contact',
       'restriction': 'all',
       'icon': 'envelope',
     }, {
