32640533669660384ded0b4386fb1b92e9950162
Lody Borgers <lody.borgers@capgemini.com>
2016-04-12 09:07:38 +0200

    Added context buttons for existing agenda items and addedAuth to controller, so that we can show the context buttons upon login

--- client/app/agenda/agenda.controller.js
+++ client/app/agenda/agenda.controller.js
@@ -1,12 +1,24 @@
 'use strict';
 
 angular.module('kidCallowayApp')
-  .controller('AgendaCtrl', function ($scope, AgendaService) {
+  .controller('AgendaCtrl', function ($scope, AgendaService, Auth) {
+    // *** SETUP
+    // Collections for the gig list and error list
     $scope.gigs = [];
     $scope.errors = [];
+    
+    // Show the editing form
     $scope.showForm = true;
+    
+    // State of the editing form: new or edit.
+    // Caption of the submit button adjusts to this state
+    $scope.formState = "new";
     $scope.submitButtonCaption = "Gig toevoegen aan agenda";
     
+    // Pass the isLoggedIn function from the Auth service to the view
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    
+    // *** GENERAL
     // Retrieve the data of all the gigs from the service
     $scope.getAllGigs = function () {
         AgendaService.get().then(function (gigs) {
@@ -16,4 +28,6 @@ angular.module('kidCallowayApp')
         });
     };
     $scope.getAllGigs();
+    
+    
   });
