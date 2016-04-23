012dccae8dbfed01d8a597f0f5514667e4d358d2
Lody Borgers <hi@lodybo.nl>
2016-04-23 22:40:37 +0200

    Switched contrib-sass to sass and updated stylesheet paths

--- Gruntfile.js
+++ Gruntfile.js
@@ -467,12 +467,11 @@ module.exports = function (grunt) {
     sass: {
       server: {
         options: {
-          loadPath: [
+          includePaths: [
             '<%= yeoman.client %>/bower_components',
             '<%= yeoman.client %>/app',
             '<%= yeoman.client %>/components'
-          ],
-          compass: false
+          ]
         },
         files: {
           '.tmp/app/app.css' : '<%= yeoman.client %>/app/app.scss'
