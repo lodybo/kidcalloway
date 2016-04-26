5a0d7c5aeb783cab5c76b296cfdd23fc9ae28f3b
Lody Borgers <lody.borgers@capgemini.com>
2016-04-26 09:37:44 +0200

    Built kid-calloway from commit 82bf154 on branch mean

---
+++ server/app.js
@@ -0,0 +1,77 @@
+/**
+ * Main application file
+ */
+
+'use strict';
+
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express = require('express');
+var mongoose = require('mongoose');
+var config = require('./config/environment');
+
+// Connect to database
+mongoose.connect(config.mongo.uri, config.mongo.options);
+
+// Populate DB with sample data
+if(config.seedDB) { require('./config/seed'); }
+
+// Setup server
+var app = express();
+var server = require('http').createServer(app);
+require('./config/express')(app);
+require('./routes')(app);
+
+// Start server
+server.listen(config.port, config.ip, function () {
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+});
+
+// Setup scheduler for the agenda
+var scheduler = require("node-schedule");
+var Agenda = require("./api/agenda/agenda.model");
+var moment = require("moment");
+
+var schedule = scheduler.scheduleJob("0 0 0 1/1 * ? *", function () {
+  // Find everything from one day back
+  var today = moment().format("YYYY-MM-DD");
+  var yesterday = moment().subtract(1, "days").format("YYYY-MM-DD");
+  
+  updateAgenda(yesterday, today);
+});
+
+// Logic for updating the agenda
+function updateAgenda(yesterday, today) {
+  Agenda.update({
+    "date.raw": {$gte: new Date(yesterday), $lt: new Date(today)}, "played": false
+  }, {
+    played: true
+  }, {
+    multi: true
+  }, function (err, response) {
+    // Check for errors
+    if (err) {
+      console.log("Error updating played gigs: ", err);
+      return;
+    }
+    
+    console.log("Succeeded in updating played gigs. Mongo's response: ", response);
+  });
+}
+
+// For testing the agenda updating logic...
+// console.log("******** Preparing to update agenda...");
+// var timeOut = setTimeout(function () {
+//   console.log("******** Setup for updating agenda...");
+//   var today = moment().format("YYYY-MM-DD");
+//   var yesterday = moment().subtract(1, "days").format("YYYY-MM-DD");
+//   console.log("yesterday: " + yesterday, "today: " + today);
+//   updateAgenda(yesterday, today);
+//   // Agenda.find({"date.raw": {$gte: new Date(yesterday), $lt: new Date(today)}, "played": false}, {}, function (err, res) {
+//   //   console.log("* Agenda.find", err, res);
+//   // });
+// }, 5000);
+
+// Expose app
+exports = module.exports = app;
