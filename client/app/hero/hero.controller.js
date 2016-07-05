c16b2545d3e2197a28c4b7138a48e251ec551f47
Lody Borgers <lody.borgers@capgemini.com>
2016-07-05 09:11:00 +0200

    Refined loader image

--- client/app/hero/hero.controller.js
+++ client/app/hero/hero.controller.js
@@ -4,11 +4,13 @@ angular.module('kidCallowayApp')
   .controller('HeroCtrl', function ($scope, SettingsService) {
     $scope.message = 'Hello';
     $scope.heroOptions = {
-      videoURL: undefined
+      videoURL: undefined,
+      animateLoader: true
     };
 
     // Get the YT url for the header
-    SettingsService.get("heroVideo").then(function(setting) {
+    SettingsService.get("heroVideo").then(function(setting) {debugger;
       $scope.heroOptions.videoURL = setting.value;
+      $scope.heroOptions.animateLoader = false;
     });
   });
