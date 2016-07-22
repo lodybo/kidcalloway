87aa0469e9a7c1f31f1613fd436a212ebf9306d6
Lody Borgers <lody.borgers@capgemini.com>
2016-07-22 15:11:25 +0200

    Spiderproofed contact details

--- client/app/contact/contact.controller.js
+++ client/app/contact/contact.controller.js
@@ -2,5 +2,14 @@
 
 angular.module('kidCallowayApp')
   .controller('ContactCtrl', function ($scope) {
-    $scope.message = 'Hello';
+    $scope.contactDetails = {
+      phone: "tel:+31 (0)6 15 07 04 08",
+      mail: "mailto:info@kidcalloway.nl"
+    };
+
+    $scope.changeContactDetails = function ($event, detail) {
+      debugger;
+      var elem = angular.element($event.currentTarget);
+      elem.attr("href", $scope.contactDetails[detail]);
+    };
   });
