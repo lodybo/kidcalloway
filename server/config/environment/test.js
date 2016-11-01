12f08b807079a1497346a54de67dd8efaa022307
Lody Borgers <hi@lodybo.nl>
2016-11-01 09:07:26 +0100

    Made sure the tests also run on CodeShip

--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -2,9 +2,15 @@
 
 // Test specific configuration
 // ===========================
+
+// DB credentials
+var user = process.env.MLAB_USER || MLAB_USER;
+var pass = process.env.MLAB_PASSWORD || MLAB_PASSWORD;
+var db = process.env.MLAB_DATABASE || MLAB_DATABASE;
+
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: "mongodb://" + process.env.MLAB_USER + ":" + process.env.MLAB_PASSWORD + "!@ds035046.mlab.com:35046/" + process.env.MLAB_DATABASE
+    uri: "mongodb://" + user + ":" + pass + "!@ds035046.mlab.com:35046/" + db
   }
 };
