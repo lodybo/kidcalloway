9916409531ead3136cb40d1728650a1583ebcb2a
Lody Borgers <hi@lodybo.nl>
2016-11-01 10:30:20 +0100

    Moved Karma browsers to environment

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration
 // http://karma-runner.github.io/0.10/config/configuration-file.html
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
     // base path, that will be used to resolve files and exclude
     basePath: '',
@@ -65,7 +65,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS'],
+    browsers: process.env.KARMA_BROWSERS.split(";"),
 
 
     // Continuous Integration mode
