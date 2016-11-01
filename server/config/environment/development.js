5373891e2ffa021b65180c5179dac7f7501dc41c
Lody Borgers <hi@lodybo.nl>
2016-11-01 09:27:11 +0100

    Another CodeShip env variable notation to test

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -2,10 +2,18 @@
 
 // Development specific configuration
 // ==================================
+var user, pass, db;
+
+/* jshint ignore:start */
+user = process.env.MLAB_USER ? process.env.MLAB_USER : ${MLAB_USER };
+pass = process.env.MLAB_USER ? process.env.MLAB_USER : ${MLAB_USER };
+db = process.env.MLAB_USER ? process.env.MLAB_USER : ${MLAB_USER };
+/* jshint ignore:end */
+
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: "mongodb://" + process.env.MLAB_USER + ":" + process.env.MLAB_PASSWORD + "!@ds035046.mlab.com:35046/" + process.env.MLAB_DATABASE
+    uri: "mongodb://" + user + ":" + pass + "!@ds035046.mlab.com:35046/" + db
   },
 
   seedDB: false
