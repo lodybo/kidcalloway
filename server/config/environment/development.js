5f24346de26cdc784b083ba71601c1660d2b691e
Lody Borgers <hi@lodybo.nl>
2016-11-01 09:34:25 +0100

    Not only changed the wrong file, but also accidentally changed the mlab url too much (port shouldn't have changed)

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -2,18 +2,10 @@
 
 // Development specific configuration
 // ==================================
-var user, pass, db;
-
-/* jshint ignore:start */
-user = process.env.MLAB_USER ? process.env.MLAB_USER : $MLAB_USER;
-pass = process.env.MLAB_USER ? process.env.MLAB_USER : $MLAB_USER;
-db = process.env.MLAB_USER ? process.env.MLAB_USER : $MLAB_USER;
-/* jshint ignore:end */
-
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: "mongodb://" + user + ":" + pass + "!@ds035046.mlab.com:35046/" + db
+    uri: "mongodb://" + process.env.MLAB_USER + ":" + process.env.MLAB_PASSWORD + "!@ds035046.mlab.com:35046/" + process.env.MLAB_DATABASE
   },
 
   seedDB: false
--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -5,6 +5,6 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: "mongodb://" + process.env.MLAB_USER + ":" + process.env.MLAB_PASSWORD + "!@ds035046.mlab.com:35046/" + process.env.MLAB_DATABASE
+    uri: "mongodb://" + process.env.MLAB_USER + ":" + process.env.MLAB_PASSWORD + "!@ds061196.mlab.com:61196/" + process.env.MLAB_DATABASE
   }
 };
