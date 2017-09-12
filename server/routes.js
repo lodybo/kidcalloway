8d97f6003a8d0679b6db6af05fed3eaf892807fd
Lody Borgers <hi@lodybo.nl>
2017-09-12 23:28:34 +0200

    Set up component sandbox

--- server/routes.js
+++ server/routes.js
@@ -8,6 +8,8 @@ var errors = require('./components/errors');
 var path = require('path');
 
 module.exports = function(app) {
+  var env = app.get('env');
+
   // Insert routes below
   app.use('/api/settings', require('./api/settings'));
   app.use('/api/agenda', require('./api/agenda'));
@@ -20,9 +22,15 @@ module.exports = function(app) {
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
 
+   if (env !== 'production') {
+     app.route('/playground').get(function(req, res) {
+       res.sendFile(path.join(__dirname, '../client/app/components/index.html'));
+     });
+   }
+
   // All other routes should redirect to the index.html
-  app.route('/*')
-  //app.route('/')
+  // app.route('/*')
+  app.route('/')
     .get(function(req, res) {
       var rootDir = process.env.NODE_ENV === 'production' ? '../public' : '../client';
       res.sendFile(path.join(__dirname, rootDir, 'index.html'));
