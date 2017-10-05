4699588aab4ed013f02ea523d119e28551e3c2e0
Lody Borgers <hi@lodybo.nl>
2017-10-05 21:18:21 +0200

    Created new discography component shell

---
+++ client/app/components/discography/discography.component.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('kidCallowayApp')
+  .component('discography', {
+    templateUrl: 'app/components/discography/discography.component.html',
+    bindings: {},
+    controller: function () {}
+  });
