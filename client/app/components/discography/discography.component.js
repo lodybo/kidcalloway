2f1c22a35cd976d80851c903bd8f60035a44629d
Lody Borgers <hi@lodybo.nl>
2017-10-05 23:22:15 +0200

    Created base component

--- client/app/components/discography/discography.component.js
+++ client/app/components/discography/discography.component.js
@@ -4,5 +4,11 @@ angular.module('kidCallowayApp')
   .component('discography', {
     templateUrl: 'app/components/discography/discography.component.html',
     bindings: {},
-    controller: function () {}
+    controller: function (DiscographyService) {
+      var ctrl = this;
+
+      DiscographyService.get('II').then(function (album) {
+        ctrl.album = album;
+      });
+    }
   });
